dcd427dd92a38e8d45bd63d231750bff63edd4c7
danifran16 <daniela.frangb@gmail.com>
2024-11-21 20:47:29 -0300

    feat: commit inicial ionic

---
+++ appanuncioscomunidad/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.appanuncioscomunidad',
+  appName: 'appanuncioscomunidad',
+  webDir: 'www'
+};
+
+export default config;
